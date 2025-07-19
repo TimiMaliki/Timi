e3fbd5933c9cb63822d71df6160c83798c1c4694
TimiMaliki <malikitimi@gmail.com>
2025-07-19 21:24:22 +0100

    services

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,12 +1,11 @@
-const Header = ({intro , title , text}) => {
+const Header = ({intro , title}) => {
     return ( 
         <div>
-              <div className="intro mb-5 md:mb-10">
+              <div className="intro mb-5 md:mb-8">
         <h4 className="text-lg font-bold flex justify-center text-center ovo-regular mb-3">
         {intro}
         </h4>
-        <h2 className="text-center text-4xl font-medium mb-6">{title}</h2>
-        <p className="text-center text-lg font-medium">{text}</p>
+        <h2 className="text-center text-4xl font-medium">{title}</h2>
       </div>
         </div>
      );
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,9 +1,66 @@
 import Header from "../components/Headers";
+import Card from "../components/card";
 
 const Services = () => {
     return ( 
-        <div className="max-w-7xl grid place-content-center mx-auto w-full py-24">
-            <Header intro={"What I offer"} title={"My Services"} text={`I am a frontend developer`}/>
+        <div className="max-w-7xl grid place-content-center mx-auto w-full py-24 ovo-regular">
+            <Header intro={"What I offer"} title={"My Services"}/>
+            <div className="text-center">
+                <h2 className="text-xl">
+            "I aim to contribute to the success of your company by <br /> leveraging my technical skills and problem-solving mindset to build impactful web solutions."
+                </h2>
+            </div>
+
+            <div className="cards grid grid-cols-4 gap-4 py-7">
+            <div className="card-one">
+            <Card
+                icon={""}
+                header={"Web design"}
+                text={
+                  "Web development is the process of building"
+                }
+                more={""}
+              />
+            </div>
+
+
+            <div className="card-two">
+            <Card
+                icon={""}
+                header={"Teaching"}
+                text={
+                  "BSc in Computer Engineering Michael Okpara University of Agriculture Umudike , Nigeria"
+                }
+                more={"CGPA - 3.54"}
+              />
+            </div>
+
+
+            <div className="card-three">
+            <Card
+                icon={""}
+                header={"UI/UX"}
+                text={
+                  "UI/UX design focuses on creating a seamless user experience"
+                }
+                more={""}
+              />
+            </div>
+
+
+
+            <div className="card-four">
+            <Card
+                icon={""}
+                header={"Graphics design"}
+                text={
+                  "Creative design solutions to enhance virtual communiction"
+                }
+                more={""}
+              />
+            </div>
+
+            </div>
         </div>
      );
 }
